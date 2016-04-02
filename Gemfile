0b77f3c7fb1e1501fafb59013d1ea8afee4dbae6
kenosome <kennyj.osorio@gmail.com>
2016-04-02 09:33:39 -0500

    se agrega el uploader carrier wave y se genera el uploader de Avatar

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'coffee-rails', '~> 4.1.0'
 
 # Administracion de autenticacion
 gem 'devise'
+# Gema para tratamiento de archivos
+gem 'carrierwave'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402135657) do
+ActiveRecord::Schema.define(version: 20160402142241) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
